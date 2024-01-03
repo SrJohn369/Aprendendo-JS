b06827f0c1fd2aa88c4cc516250697cc55b53de3
SrJohn963 <srjohn963@gmail.com>
2024-01-03 17:42:15 -0300

    Estudados Array e Ojestos

---
+++ js/array.js
@@ -0,0 +1,20 @@
+//Declarenco um array
+let frutas = [
+    'maça', 'banana', 'laranja'
+];
+
+// para acessar aelementos
+let elemento_1 = frutas[0];
+console.log(elemento_1);
+
+// alterar elemetos
+frutas[0] = 'pêra';
+console.log(frutas[0]);
+
+// Adição de elementos
+frutas.push('Morango');
+console.log(frutas);
+
+// Comprimento do arry
+let tamnahdo_do_array = frutas.length;
+console.log(tamnahdo_do_array);
---
+++ js/objetos.js
@@ -0,0 +1,16 @@
+var objeto = {
+    'Nome': "José",
+    'Idade': 56
+};
+// Pegar dados
+var nome = objeto.Nome;
+console.log(nome);
+
+// Mudar nome 
+objeto.Nome = 'Ronaldo';
+console.log(objeto.Nome);
+
+// Adicionar um novo atributo
+objeto.Profissao = 'Agricultor';
+console.log(objeto.Profissao);
+console.log(objeto);
