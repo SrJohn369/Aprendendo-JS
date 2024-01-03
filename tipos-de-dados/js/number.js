9c71e82e0175333edded19fd61920d77cf445fac
SrJohn963 <srjohn963@gmail.com>
2024-01-03 15:48:00 -0300

    Iniciando este repositorio

---
+++ tipos-de-dados/js/number.js
@@ -0,0 +1,17 @@
+console.log("var numero = 5;");
+var numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+console.log("Numero float também é Number");
+var numero_float = 5.78;
+
+console.log(numero_float);
+console.log(typeof numero_float);
+
+console.log("Simbolics numbers");
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
